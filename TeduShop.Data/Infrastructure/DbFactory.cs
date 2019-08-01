36f8d374c694f969e2dcdb3faa12423b7aad4c0b
việt đỗ hoàng <vietdohoang1997@gmail.com>
2019-08-01 10:14:33 +0700

    First  commit

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext _dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return _dbContext ?? (_dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (_dbContext != null)
+                _dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TeduShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TeduShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f207fb5f-42d4-46e5-b2ee-7433ca9ef200")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
